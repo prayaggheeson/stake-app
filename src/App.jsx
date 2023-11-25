d987c96edbf389a4f1c5fab4e3199036f06a1200
prayaggheeson <prayag.gheeson@gmail.com>
2023-11-25 14:50:44 +0530

    added staking, unstakeing, claim and deposit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import { ConnectWallet, useContract, useTokenBalance, useAddress } from "@thirdweb-dev/react"
+import { ConnectWallet, useContract, useTokenBalance, useAddress } from "@thirdweb-dev/react";
 import Stake from "./component/Stake";
-
 
 function App() {
   const address = useAddress();
 
   const { contract: rewardTokenContract } = useContract("0xE2678a381543Af153165abC3b3F88622CE315EE8");
-  const { data: rewardtokenBalance, isLoading: isLoadingTokenBalance  } = useTokenBalance(rewardTokenContract, address);
-
-  
- 
+  const { data: rewardtokenBalance, isLoading: isLoadingTokenBalance } = useTokenBalance(rewardTokenContract, address);
 
   const { contract: stakeTokenContract } = useContract("0x32702946083578B514853528119Ecf2a7f0cd664");
   const { data: stakeTokenBalance, isLoading: isLoadingStakeTokenBalance } = useTokenBalance(stakeTokenContract, address);
- 
+
   return (
-    <>
-     <h1 className='text-4xl text-sky-400 text-center'>DeFi <span className="text-black">Staking App</span></h1>
-     <ConnectWallet className="justify-center"/>
-     <div className="bg-gray-500 p-8 text-white">
-      <h2 className="text-2xl text-white">{rewardtokenBalance?.name}</h2>
-      {isLoadingTokenBalance ? <h5>is loading</h5> : <h5>User Balance:{"  "} {rewardtokenBalance?.displayValue}</h5>}
-      {isLoadingTokenBalance ? <h5>is loading</h5> : <h5>Token Symbol:{"  "} {rewardtokenBalance?.symbol}</h5>}      
-     </div>
-    
-
-     <div className="bg-gray-400 p-8 text-white">
-      <h2 className="text-2xl text-white">{stakeTokenBalance?.name}</h2>
-      
-      {isLoadingStakeTokenBalance ? <h5>is loading</h5> : <h5>Stake Token Balance:{"  "} {stakeTokenBalance?.displayValue}</h5>}
-      {isLoadingStakeTokenBalance ? <h5>is loading</h5> : <h5>Token Symbol:{"  "} {stakeTokenBalance?.symbol}</h5>}      
+    <div className="bg-gray-100 min-h-screen p-4">
+    <div className="flex flex-col items-center md:items-start md:flex-row justify-between mb-4">
+      <h1 className="text-4xl text-sky-400 text-center mb-4 md:mb-0">DeFi <span className="text-black">Staking App</span></h1>
+      <div className="md:right-0">
+        <ConnectWallet className="" />
       </div>
-
-
-      <div className="bg-yellow-500">
-      <h2 className="text-2xl text-center">Stake your {stakeTokenBalance?.name} And Get in Rewards get {rewardtokenBalance?.name} </h2>
-
-     <Stake/>
-     </div>
-    </>
-  )
+    </div>
+  
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="bg-gray-900 p-8 text-white mb-4 rounded-md">
+        <h2 className="text-2xl">{rewardtokenBalance?.name}</h2>
+        {isLoadingTokenBalance ? <h5>Loading...</h5> : <h5>User Balance:{"  "} {rewardtokenBalance?.displayValue}</h5>}
+        {isLoadingTokenBalance ? <h5>Loading...</h5> : <h5>Token Symbol:{"  "} {rewardtokenBalance?.symbol}</h5>}
+      </div>
+  
+      <div className="bg-gray-900 p-8 text-white mb-4 rounded-md">
+        <h2 className="text-2xl">{stakeTokenBalance?.name}</h2>
+        {isLoadingStakeTokenBalance ? <h5>Loading...</h5> : <h5>Stake Token Balance:{"  "} {stakeTokenBalance?.displayValue}</h5>}
+        {isLoadingStakeTokenBalance ? <h5>Loading...</h5> : <h5>Token Symbol:{"  "} {stakeTokenBalance?.symbol}</h5>}
+      </div>
+    </div>
+  
+    <div className="p-8 rounded-md">
+      <h2 className="text-2xl text-center mb-4">Stake your {stakeTokenBalance?.name} And Get in Rewards get {rewardtokenBalance?.name} </h2>
+      <Stake />
+    </div>
+  </div>
+  
+  );
 }
 
-export default App
+export default App;
--- src/component/Stake.jsx
+++ src/component/Stake.jsx
@@ -1,26 +1,32 @@
-import  { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   useAddress,
   useContract,
   useContractRead,
   useTokenBalance,
+  Web3Button,
 } from "@thirdweb-dev/react";
+import {
+  STAKE_CONTRACT_ADDRESSES,
+  STAKE_TOKEN_ADDRESSES,
+  REWARD_TOKEN_ADDRESSES,
+} from "./Constants";
 import { ethers } from "ethers";
 
 const Stake = () => {
-  const [stakeAmount, setStakeAmount] = useState("0");
   const address = useAddress();
-  const { contract: stakeContract } = useContract(
-    "0x6A49c2701626e749743483cA8599c46D7069834D",
+
+  const { contract: stakeTokenContract } = useContract(
+    STAKE_TOKEN_ADDRESSES,
     "token"
   );
   const { contract: rewardTokenContract } = useContract(
-    "0xE2678a381543Af153165abC3b3F88622CE315EE8",
-    "custom"
-  );
-  const { contract: stakeTokenContract } = useContract(
-    "0x32702946083578B514853528119Ecf2a7f0cd664",
+    REWARD_TOKEN_ADDRESSES,
     "token"
+  );
+  const { contract: stakeContract } = useContract(
+    STAKE_CONTRACT_ADDRESSES,
+    "custom"
   );
 
   const {
@@ -36,70 +42,135 @@ const Stake = () => {
     useTokenBalance(rewardTokenContract, address);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    setInterval(() => {
       refetchStakeInfo();
     }, 10000);
-
-    return () => clearInterval(intervalId);
   }, []);
 
-  async function handleStake() {
-    try {
-      await stakeTokenContract?.erc20.setAllowance(stakeContract, stakeAmount);
-      await stakeContract.call("stake", [
-        ethers.utils.parseEther(stakeAmount),
-      ]);
-      resetValue();
-    } catch (error) {
-      console.error("Error while staking:", error);
-    }
-  }
-
-  function handleClaimRewards() {
-    try {
-      stakeContract.call("claimRewards");
-      resetValue();
-    } catch (error) {
-      console.error("Error while claiming rewards:", error);
-    }
-  }
+  const [stakeAmount, setStakeAmount] = useState("0");
+  const [unstakeAmount, setUnstakeAmount] = useState("0");
+  const [depositAmount, setDepositAmount] = useState("0"); // New state for deposit amount
 
   function resetValue() {
     setStakeAmount("0");
+    setUnstakeAmount("0");
+    setDepositAmount("0");
   }
 
   return (
-    <div>
-      <div>
-        <h2>Stake token balance:</h2>
-        {loadingStakeInfo || loadingStakeTokenBalance ? (
-          <p>Loading...</p>
-        ) : (
-          <p>
-            {ethers.utils.formatEther(stakeInfo[0])} {stakeTokenBalance?.symbol}
-          </p>
-        )}
-      </div>
-      <div>
-        <input
-          type="number"
-          max={stakeTokenBalance?.displayValue}
-          value={stakeAmount}
-          onChange={(e) => setStakeAmount(e.target.value)}
-        />
-        <button onClick={handleStake}>Stake</button>
-      </div>
-      <div>
-        <h2>Reward token balance:</h2>
-        {loadingStakeInfo || loadingRewardTokenBalance ? (
-          <p>Loading...</p>
-        ) : (
-          <p>
-            {ethers.utils.formatEther(stakeInfo[1])}{" "}
-            {rewardTokenBalance?.symbol}
-          </p>
-        )}
-        <button onClick={handleClaimRewards}>Claim</button>
+    <div className="container mx-auto p-4 bg-gray-900 rounded-xl text-white">
+      <h1 className="text-3xl text-center text-blue-500 font-bold mb-4">
+        Stake Your Token
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {/* Stake and Unstake Token Sections */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+          {/* Stake Token Section */}
+          <div className="border w-full md:w-auto p-4 flex flex-col rounded-md bg-gray-800">
+            <div>
+              <h3 className="text-green-500 text-xl mb-2">Stake Token:</h3>
+              {loadingStakeInfo || loadingStakeTokenBalance ? (
+                <p className="text-white">Loading...</p>
+              ) : (
+                <p className="text-white">
+                  {ethers.utils.formatEther(stakeInfo[0])}{" "}
+                  {stakeTokenBalance?.symbol}
+                </p>
+              )}
+            </div>
+
+            <div className="mt-4 space-y-2">
+              <input
+                type="number"
+                max={stakeTokenBalance?.displayValue}
+                value={stakeAmount}
+                onChange={(e) => setStakeAmount(e.target.value)}
+                className="border p-2 rounded-md bg-gray-700 text-white"
+              />
+              <button
+                onClick={async () => {
+                  await stakeTokenContract?.erc20.setAllowance(
+                    STAKE_CONTRACT_ADDRESSES,
+                    stakeAmount
+                  );
+                  await stakeContract.call("stake", [
+                    ethers.utils.parseEther(stakeAmount),
+                  ]);
+                  resetValue();
+                }}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+              >
+                Stake
+              </button>
+            </div>
+          </div>
+
+          {/* Unstake token section */}
+          <div className="mt-4 w-full md:w-auto space-y-2 border rounded-lg p-4 bg-gray-800">
+            <h3 className="text-red-500 text-xl mb-2">Unstake Token:</h3>
+            <div>
+              <input
+                type="number"
+                value={unstakeAmount}
+                onChange={(e) => setUnstakeAmount(e.target.value)}
+                className="border p-2 rounded-md bg-gray-700 text-white"
+              />
+              <button
+                onClick={async () => {
+                  await stakeContract.call("withdraw", [
+                    ethers.utils.parseEther(unstakeAmount),
+                  ]);
+                  resetValue();
+                }}
+                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+              >
+                Unstake
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Reward Token Section */}
+        <div className="border p-4 rounded-md bg-gray-800 col-span-2">
+          <h3 className="text-yellow-500">Reward Token:</h3>
+          {loadingStakeInfo || loadingRewardTokenBalance ? (
+            <p>Loading...</p>
+          ) : (
+            <p>
+              {ethers.utils.formatEther(stakeInfo[1])}{" "}
+              {rewardTokenBalance?.symbol}
+            </p>
+          )}
+
+          <div className="mt-4 space-y-2">
+            <input
+              type="number"
+              value={depositAmount}
+              onChange={(e) => setDepositAmount(e.target.value)}
+              className="border p-2 rounded-md bg-gray-700 text-white"
+            />
+            <Web3Button
+              contractAddress="0x6A49c2701626e749743483cA8599c46D7069834D"
+              action={(contract) => {
+                contract.call("depositRewardTokens", [depositAmount]);
+              }}
+              className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
+            >
+              Deposit Reward Tokens
+            </Web3Button>
+          </div>
+
+          <button
+            onClick={async () => {
+              await stakeContract.call("claimRewards");
+              resetValue();
+            }}
+            className="mt-4 bg-purple-500 text-white px-4 py-2 rounded-md hover:bg-purple-600"
+          >
+            Claim
+          </button>
+        </div>
       </div>
     </div>
   );
